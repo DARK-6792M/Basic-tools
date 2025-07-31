29d0b810466755221c9be8f948b3a76cb260904f
DARK-6792M <mosharofhossain52.mh91@gmail.com>
2025-08-01 02:28:57 +0300

    Add files via upload

---
+++ tools make.py
@@ -0,0 +1,77 @@
+
+import os
+import sys
+import time
+os.system("clear")
+print("\n\n\n")
+cd=" 		\033[1;32msystem loading........"
+for d in cd:
+	sys.stdout.write(d)
+	sys.stdout.flush()
+	time.sleep(0.2)
+	
+print("\n\n\n")
+time.sleep(3)
+os.system("clear")
+print("\n\n\n")
+cd=" 		\033[1;33msuccessfully loaded\n"
+
+for d in cd:
+	sys.stdout.write(d)
+	sys.stdout.flush()
+	time.sleep(0.2)
+print("\n\n\n")
+name=input("	\033[1;34myour Name:")
+
+cd=" 		\033[1;35mHey " +name+",be Ethical\n"
+for d in cd:
+	sys.stdout.write(d)
+	sys.stdout.flush()
+	time.sleep(0.2)
+print("\n\n\n")
+time.sleep(2)
+os.system("clear")
+print("""
+========--------+--------=====================\033[1;36m
+____         __  __ _____ _____  
+  / ____|  /\\   |  \\/  |_   _|  __ \\ 
+ | (___   /  \\  | \\  / | | | | |__) |
+  \\___ \\ / /\\ \\ | |\\/| | | | |  _  / 
+  ____) / ____ \\| |  | |_| |_| | \\ \\ 
+ |_____/_/    \\_\\_|  |_|_____|_|  \\_\\
+
+===============---------+---------------==============
+Owner :SAMIR STLP
+Github :something
+Facebook:STLP
+Use only For Educationl purposes.
+==============---------=================
+""")
+
+
+#basic pacage installer
+print("\033[1;31m")
+os.system("pkg update")
+print("\033[1;32m")
+os.system("pkg upgrade")
+print("\033[1;33m")
+os.system("pkg install git")
+print("\033[1;34m")
+os.system("pkg install ruby")
+print("\033[1;35m")
+os.system("gem install lolcat")
+print("\033[1;36m")
+os.system("pip install requests ")
+os.system("pkg install php ")
+print("\033[1;38m")
+os.system("pip2 install requests ")
+os.system("pip2 install mechanize ")
+os.system("pkg install nmap ")
+os.system("pkg install wget ")
+os.system("pkg install golang ")
+os.system("pkg install perl ")
+os.system("pkg install nodejs ")
+print("\033[1;34m")
+print("all pakage install succesfully")
+
+
